3694f126f0db226ad35179d799d5c1107d513d83
andrey.korlyuk <andrey.korlyuk@gmail.com>
2018-01-06 14:49:55 +0200

    extend commons format

--- src/main/g8/src/main/scala/com/botchaa/$name;format="word"$/json/$name;format="Camel"$Protocol.scala
+++ src/main/g8/src/main/scala/com/botchaa/$name;format="word"$/json/$name;format="Camel"$Protocol.scala
@@ -1,5 +1,5 @@
 package com.botchaa.$name;format="word"$.json
 
-trait $name;format="Camel"$Protocol {}
+trait $name;format="Camel"$Protocol extends CommonsJsonProtocol with DefaultJsonProtocol {}
 
 object $name;format="Camel"$Protocol extends $name;format="Camel"$Protocol
